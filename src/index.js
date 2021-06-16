d38cb0e78a4a1e1141262eb46c2d25d4d7feb5d9
fredsalas87 <fredsalas87@gmail.com>
2021-06-16 19:16:32 -0300

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,122 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./index.css";
 
 function CardItem(props) {
   return (
-    <div className="card_item">
-      <h2>{props.nome}</h2>
-      <img src={props.imagem} alt="Imagem da personagem" width="300px" />
-    </div>
+    <Route
+      render={({ history }) => (
+        <div
+          className="card_item"
+          onClick={() => {
+            history.push("/visualizar/" + props.item._id);
+          }}
+        >
+          <h2>{props.item.nome}</h2>
+          <img src={props.item.imagem} alt={props.item.nome} width="300" />
+        </div>
+      )}
+    />
   );
 }
 
-function ListarItens() {
-  return (
-    <div className="lista_itens">
-      <CardItem
-        nome="Mario Bros"
-        imagem="https://i.pinimg.com/474x/b4/ab/d7/b4abd78a2e58557bea252a50c48a746b.jpg"
-      />
-      <CardItem
-        nome="Luigi Bros"
-        imagem="https://www.imagenspng.com.br/wp-content/uploads/2015/02/super-mario-luigi-03.png"
-      />
-    </div>
-  );
+class ListarItens extends React.Component {
+  // 1 - Definir estado inicial
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      itens: [],
+    };
+  }
+
+  async componentDidMount() {
+    const request = await fetch("https://backend-flexivel.herokuapp.com/", {
+      headers: new Headers({
+        Authorization: "120609",
+      }),
+    });
+
+    const json = await request.json();
+
+    // 2 - Atualiza o estado
+    this.setState({
+      itens: json,
+    });
+  }
+
+  render() {
+    // 3 - Renderiza utilizando a informação que está no estado
+    return (
+      <div className="lista_itens">
+        {this.state.itens.map((item, index) => (
+          <CardItem item={item} key={index} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class VisualizarItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.id = this.props.match.params.id;
+
+    this.state = {
+      item: {},
+    };
+  }
+
+  async componentDidMount() {
+    const request = await fetch(
+      "https://backend-flexivel.herokuapp.com/" + this.id,
+      {
+        headers: new Headers({
+          Authorization: "120609",
+        }),
+      }
+    );
+
+    const json = await request.json();
+
+    // 2 - Atualiza o estado
+    this.setState({
+      item: json,
+    });
+  }
+
+  render() {
+    return (
+      <div className="card_item">
+        <h2>{this.state.item.nome}</h2>
+        <img
+          src={this.state.item.imagem}
+          alt={this.state.item.nome}
+          width="300"
+        />
+        <div>{this.state.item.descricao}</div>
+      </div>
+    );
+  }
 }
 
 function App() {
-  return <ListarItens />;
+  return (
+    <Switch>
+      <Route path="/" exact={true} component={ListarItens} />
+
+      <Route path="/visualizar/:id" component={VisualizarItem} />
+    </Switch>
+  );
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
